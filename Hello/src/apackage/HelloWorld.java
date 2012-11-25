6916fe522db5ef9fcac82dc44c417ffb56fd0e3b
vlad.lep@gmail.com <vlad.lep@gmail.com@42cb9919-5284-5739-a0a0-aee2a01bd16e>
2012-11-25 22:01:03 +0000

    add cases to cycolmetic complexity partly duplication - incorrect for now add code to the hello project

--- Hello/src/apackage/HelloWorld.java
+++ Hello/src/apackage/HelloWorld.java
@@ -3,7 +3,9 @@ package apackage;
 public class HelloWorld {
 
 	public HelloWorld() {
-		// TODO Auto-generated constructor stub
+		int a = 1<2 ? 4:5;
+		if( a== 1 && a <2)
+			a =2;
 	}
 
 	/**
@@ -11,6 +13,25 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+	}
+
+	public static void main2(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 		System.out.println("hello");
 	}
 
--- Hello/src/apackage/MainClass.java
+++ Hello/src/apackage/MainClass.java
@@ -11,7 +11,7 @@ public class MainClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 1<2 ? 4:5;
 	}
 
 }
